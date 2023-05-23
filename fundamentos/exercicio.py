1090a969ed8c0b7ef44519a143c492300f5b6ea2
Samuel Villain <samuelsvillain@gmail.com>
2023-05-23 18:23:35 -0300

    Funções

--- fundamentos/exercicio.py
+++ fundamentos/exercicio.py
@@ -21,6 +21,16 @@ def imprimir_matriz(matriz):
     for vetor in matriz:
         print(vetor)
 
+def Passar_Posicoes(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            i = int(input("Digite um valor: "))
+            j = int(input("Digite um valor: "))
+            valor = input("Digite um valor: ")
+            matriz[i][j] = valor
+            return valor
+
+
 
 
 def funcao_teste(nome, sobrenome, pqp):
@@ -28,8 +38,9 @@ def funcao_teste(nome, sobrenome, pqp):
 
 if __name__ == "__main__":
     funcao_teste("Villain","Roberto", 21)
-    # matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     # alterar_diagonal_principal(matriz, 10)
     # diagonal_secundaria(matriz, 20)
-    # imprimir_matriz(matriz)
+    Passar_Posicoes(matriz)
+    imprimir_matriz(matriz)
 
