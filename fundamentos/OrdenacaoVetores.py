c4f1c8214461fac5fb1d3969e625ff8e2803de75
samuel <samuelsvillain@gmail.com>
2023-05-24 00:36:54 -0300

    exercicio 1 concluido

--- fundamentos/OrdenacaoVetores.py
+++ fundamentos/OrdenacaoVetores.py
@@ -1,15 +1,26 @@
-nome = ["S","a","m","u","e","l"]
+# nome = ["S","a","m","u","e","l"]
+# tamanho_vetor = len(nome)
+# for i in range(tamanho_vetor):
+#     ultima_posicao = tamanho_vetor-(1+i)
+#     bola_da_vez = nome[i]
+#     rede = nome[ultima_posicao]
+#     if ultima_posicao == 2:
+#         break
+#     nome[ultima_posicao] = bola_da_vez
+#     nome[i] = rede
+#     print(nome)
 
-for i in range(len(nome)):
-    tamanho_vetor = len(nome)
-    ultima = tamanho_vetor-(1+i)
-    auxiliar = nome[i]
-    auxiliar2 = nome[ultima]
-
-    nome[ultima] = auxiliar
-    nome[i] = auxiliar2
-    print(nome)
 
 
 #ordenar o vetor [9, 1, 7, 3, 5, 2, 10, 4, 6, 8]
+numero = [9, 1, 7, 3, 5, 2, 10, 4, 6, 8]
 
+
+vetor_tamanho = len(numero)
+for i in range(vetor_tamanho):
+    posicao_dois = vetor_tamanho-(9+i)
+    bola_de_futebol = numero[i]
+    gol = numero[posicao_dois]
+    numero[posicao_dois] = bola_de_futebol
+    numero[i] = gol
+    print(numero)
